10315cd72a7530a3f6726d082cf6231e720ad2eb
Anmol das <johnsun2618@gmail.com>
2023-04-04 18:16:59 +0530

    attempt 6

--- src/main/java/com/driver/controllers/AirportController.java
+++ src/main/java/com/driver/controllers/AirportController.java
@@ -87,10 +87,6 @@ public class AirportController {
         //Calculate the total number of people who have flights on that day on a particular airport
         //This includes both the people who have come for a flight and who have landed on an airport after their flight
 
-        if (date == null || airportName == null || airportName.isEmpty()) {
-            throw new IllegalArgumentException("Invalid date or airport name.");
-        }
-
         int totalPeople = 0;
 
         for (Flight flight : flights) {
@@ -98,7 +94,6 @@ public class AirportController {
                 totalPeople += flight.getMaxCapacity();
             }
         }
-
         return totalPeople;
 
     }
@@ -178,7 +173,7 @@ public class AirportController {
         //Tell the count of flight bookings done by a passenger: This will tell the total count of flight bookings done by a passenger :
         return (int) bookings.values().stream()
                 .flatMap(List::stream)
-                .filter(id -> id.equals(passengerId))
+                .filter(booking -> false)
                 .count();
     }
 
